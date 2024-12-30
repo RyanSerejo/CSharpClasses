28044fee2aeca9579a7ca1f5baa083f2629c9045
RyanSerejo <140823759+RyanSerejo@users.noreply.github.com>
2024-12-30 16:08:50 -0300

    aula66

--- Parte 2/Componentes/F_checkBox.Designer.cs
+++ Parte 2/Componentes/F_checkBox.Designer.cs
@@ -28,22 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.cb_carro = new System.Windows.Forms.CheckBox();
+            this.cb_aviao = new System.Windows.Forms.CheckBox();
+            this.cb_navio = new System.Windows.Forms.CheckBox();
+            this.cb_onibus = new System.Windows.Forms.CheckBox();
+            this.btn_checkedTransp = new System.Windows.Forms.Button();
+            this.cb_patinete = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
+            // 
+            // cb_carro
+            // 
+            this.cb_carro.AutoSize = true;
+            this.cb_carro.Location = new System.Drawing.Point(14, 10);
+            this.cb_carro.Name = "cb_carro";
+            this.cb_carro.Size = new System.Drawing.Size(51, 17);
+            this.cb_carro.TabIndex = 0;
+            this.cb_carro.Text = "Carro";
+            this.cb_carro.UseVisualStyleBackColor = true;
+            // 
+            // cb_aviao
+            // 
+            this.cb_aviao.AutoSize = true;
+            this.cb_aviao.Location = new System.Drawing.Point(14, 33);
+            this.cb_aviao.Name = "cb_aviao";
+            this.cb_aviao.Size = new System.Drawing.Size(53, 17);
+            this.cb_aviao.TabIndex = 1;
+            this.cb_aviao.Text = "Avião";
+            this.cb_aviao.UseVisualStyleBackColor = true;
+            // 
+            // cb_navio
+            // 
+            this.cb_navio.AutoSize = true;
+            this.cb_navio.Location = new System.Drawing.Point(14, 56);
+            this.cb_navio.Name = "cb_navio";
+            this.cb_navio.Size = new System.Drawing.Size(54, 17);
+            this.cb_navio.TabIndex = 2;
+            this.cb_navio.Text = "Navio";
+            this.cb_navio.UseVisualStyleBackColor = true;
+            // 
+            // cb_onibus
+            // 
+            this.cb_onibus.AutoSize = true;
+            this.cb_onibus.Location = new System.Drawing.Point(14, 79);
+            this.cb_onibus.Name = "cb_onibus";
+            this.cb_onibus.Size = new System.Drawing.Size(59, 17);
+            this.cb_onibus.TabIndex = 3;
+            this.cb_onibus.Text = "Ônibus";
+            this.cb_onibus.UseVisualStyleBackColor = true;
+            // 
+            // btn_checkedTransp
+            // 
+            this.btn_checkedTransp.Location = new System.Drawing.Point(155, 10);
+            this.btn_checkedTransp.Name = "btn_checkedTransp";
+            this.btn_checkedTransp.Size = new System.Drawing.Size(239, 27);
+            this.btn_checkedTransp.TabIndex = 4;
+            this.btn_checkedTransp.Text = "Transportes marcados";
+            this.btn_checkedTransp.UseVisualStyleBackColor = true;
+            this.btn_checkedTransp.Click += new System.EventHandler(this.btn_checkedTransp_Click);
+            // 
+            // cb_patinete
+            // 
+            this.cb_patinete.AutoSize = true;
+            this.cb_patinete.Location = new System.Drawing.Point(14, 129);
+            this.cb_patinete.Name = "cb_patinete";
+            this.cb_patinete.Size = new System.Drawing.Size(65, 17);
+            this.cb_patinete.TabIndex = 5;
+            this.cb_patinete.Text = "Patinete";
+            this.cb_patinete.UseVisualStyleBackColor = true;
+            this.cb_patinete.CheckedChanged += new System.EventHandler(this.cb_patinete_CheckedChanged);
             // 
             // F_CheckBox
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(473, 450);
+            this.Controls.Add(this.cb_patinete);
+            this.Controls.Add(this.btn_checkedTransp);
+            this.Controls.Add(this.cb_onibus);
+            this.Controls.Add(this.cb_navio);
+            this.Controls.Add(this.cb_aviao);
+            this.Controls.Add(this.cb_carro);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.Name = "F_CheckBox";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CheckBox";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox cb_carro;
+        private System.Windows.Forms.CheckBox cb_aviao;
+        private System.Windows.Forms.CheckBox cb_navio;
+        private System.Windows.Forms.CheckBox cb_onibus;
+        private System.Windows.Forms.Button btn_checkedTransp;
+        private System.Windows.Forms.CheckBox cb_patinete;
     }
 }
--- Parte 2/Componentes/F_checkBox.cs
+++ Parte 2/Componentes/F_checkBox.cs
@@ -12,9 +12,36 @@ namespace Componentes
 {
     public partial class F_CheckBox : Form
     {
+        List<CheckBox> checkedBoxes = new List<CheckBox>();
         public F_CheckBox()
         {
             InitializeComponent();
+            checkedBoxes.Add(cb_carro);
+            checkedBoxes.Add(cb_aviao);
+            checkedBoxes.Add(cb_navio);
+            checkedBoxes.Add(cb_onibus);
+        }
+
+        private void btn_checkedTransp_Click(object sender, EventArgs e)
+        {
+            string txt = "";
+            foreach (CheckBox t in checkedBoxes) { 
+                if (t.Checked)
+                {
+                    txt += t.Text + ", ";
+                }
+            }
+            MessageBox.Show(txt);
+        }
+
+        private void cb_patinete_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cb_patinete.Checked){
+            
+                MessageBox.Show("Patinete marcado");
+            
+            }
+
         }
     }
 }
