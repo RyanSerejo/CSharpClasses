050ee723405f92f7033547414d3b07d52f97f702
RyanSerejo <140823759+RyanSerejo@users.noreply.github.com>
2025-02-02 17:57:47 -0300

    Aula101

---
+++ Parte 2/AcademiaProject/Banco.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;//*
+using System.Data;//*Esses importes foram adicionados depois da criação
+namespace AcademiaProject
+{
+    internal class Banco
+    {
+        private static SQLiteConnection conexao;
+
+        private static SQLiteConnection ConexaoBanco()
+        {
+            conexao = new SQLiteConnection("Data Source:C:\\Users\\Ryan\\Documents\\C#\\CSharpClasses\\Parte 2\\AcademiaProject\\banco\\banco_academia.db");
+            conexao.Open();
+            return conexao;
+        }
+    }
+}
--- Parte 2/AcademiaProject/Form1.Designer.cs
+++ Parte 2/AcademiaProject/Form1.Designer.cs
@@ -28,20 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pb_ledLogado = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb_acesso = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lb_nomeUsuario = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_ledLogado)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Controls.Add(this.lb_nomeUsuario);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.lb_acesso);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pb_ledLogado);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 453);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(794, 38);
+            this.panel1.TabIndex = 0;
+            // 
+            // pb_ledLogado
+            // 
+            this.pb_ledLogado.Image = global::AcademiaProject.Properties.Resources.redLed;
+            this.pb_ledLogado.Location = new System.Drawing.Point(3, 3);
+            this.pb_ledLogado.Name = "pb_ledLogado";
+            this.pb_ledLogado.Size = new System.Drawing.Size(30, 30);
+            this.pb_ledLogado.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_ledLogado.TabIndex = 0;
+            this.pb_ledLogado.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Acesso:";
+            // 
+            // lb_acesso
+            // 
+            this.lb_acesso.AutoSize = true;
+            this.lb_acesso.Location = new System.Drawing.Point(78, 12);
+            this.lb_acesso.Name = "lb_acesso";
+            this.lb_acesso.Size = new System.Drawing.Size(13, 13);
+            this.lb_acesso.TabIndex = 2;
+            this.lb_acesso.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(106, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Usuário:";
+            // 
+            // lb_nomeUsuario
+            // 
+            this.lb_nomeUsuario.AutoSize = true;
+            this.lb_nomeUsuario.Location = new System.Drawing.Point(149, 12);
+            this.lb_nomeUsuario.Name = "lb_nomeUsuario";
+            this.lb_nomeUsuario.Size = new System.Drawing.Size(16, 13);
+            this.lb_nomeUsuario.TabIndex = 5;
+            this.lb_nomeUsuario.Text = "---";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(794, 491);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Text = " ";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RealGym - v 1.0";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_ledLogado)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.PictureBox pb_ledLogado;
+        private System.Windows.Forms.Label lb_nomeUsuario;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lb_acesso;
     }
 }
 
